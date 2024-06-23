26950690d1fb4d11cdd2033ab227dda9d2940b63
Josecarlosdoss <rendaextra909090@gmail.com>
2024-06-23 02:31:44 -0300

    novo teste js

---
+++ aula12/modelo01.js
@@ -0,0 +1,28 @@
+function carregar() {
+
+            var msg = window.document.getElementById('msg')
+            
+            var image = window.document.getElementById('image')
+            
+            var data = new Date()
+            
+            var hora = data.getHours()
+            
+            msg.innerHTML = `agora são ${hora} hora.`
+            
+            if (hora >= 0 && < 12) {
+                //Bom dia!
+                img.src = 'foto-manha.jpg'
+                document.body.style.background = '#D8C7A3'
+            } else if (hora >= 12 && < 18) {
+                //Boa tarde!
+                img.src = 'foto-tarde.jpg'
+                document.body.style.background = '#B96A52'
+                
+            } else {
+                //Boa noite!
+                img.src = 'foto-noite.jpg'
+                document.body.style.background = '#0367A6'
+            }
+
+}

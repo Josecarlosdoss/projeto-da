b73ddacf51bdb3cc6285d471b454a9ae41f7ce17
José Carlos <rendaextra909090@gmail.com>
2024-06-23 19:38:30 -0300

    Update modelo01.js

--- aula12/modelo01.js
+++ aula12/modelo01.js
@@ -1,20 +1,20 @@
-function carregar() {
+   function carregar() {
 
             var msg = window.document.getElementById('msg')
             
-            var image = window.document.getElementById('image')
+            var img = window.document.getElementById('image')
             
             var data = new Date()
             
             var hora = data.getHours()
             
-            msg.innerHTML = (`agora são ${hora} hora.`)
+            msg.innerHTML = `agora são ${hora} hora.`
             
-            if (hora >= 0 && < 12) {
+            if (hora >= 0 && hora < 12) {
                 //Bom dia!
                 img.src = 'foto-manha.jpg'
                 document.body.style.background = '#D8C7A3'
-            } else if (hora >= 12 && < 18) {
+            } else if (hora >= 12 && hora <= 18) {
                 //Boa tarde!
                 img.src = 'foto-tarde.jpg'
                 document.body.style.background = '#B96A52'
@@ -26,3 +26,5 @@ function carregar() {
             }
 
 }
+
+        

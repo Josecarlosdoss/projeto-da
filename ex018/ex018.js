9feb7bc3e54073f01444c59785587d8bdc90e104
José Carlos <rendaextra909090@gmail.com>
2024-07-05 16:53:50 -0300

    Update ex018.js

--- ex018/ex018.js
+++ ex018/ex018.js
@@ -1,6 +1,6 @@
 let num = document.querySelector('input#fnum')
-let lista = document.queySelector('select#flista')
-let res = document.queySelector('div#res')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
 let valores = [ ]
 
 function isNumero(n) {

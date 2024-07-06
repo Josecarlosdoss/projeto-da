1e0ec01b4861957762cedb8fbb26d03a841980f0
José Carlos <rendaextra909090@gmail.com>
2024-07-05 23:16:47 -0300

    Update ex018.js

--- ex018/ex018.js
+++ ex018/ex018.js
@@ -12,8 +12,8 @@ function isNumero(n) {
             
 }
 
-function inLista(n, 1) {
-            if (1.indexOf(Number(n)) != -1) {
+function inLista(n, l) {
+            if (l.indexOf(Number(n)) != -1) {
                 return true 
             
             } else {
